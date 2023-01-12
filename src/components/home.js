aa0649b81246b38acc32e70ab754b2d5a76c5965
jiseok12 <103105145+jiseok12@users.noreply.github.com>
2023-01-12 19:32:54 +0900

    add:: all page

--- src/components/home.js
+++ src/components/home.js
@@ -2,6 +2,10 @@ import '../styles/home.css';
 import React from 'react';
 import Header from './header';
 import { BsFillPeopleFill } from "react-icons/bs";
+import { AiOutlineUser } from "react-icons/ai";
+import { MdEmojiPeople } from "react-icons/md";
+import { AiOutlineClockCircle } from "react-icons/ai";
+import Rechart from "./Rechart";
 
 function Home(){
     return(
@@ -15,11 +19,25 @@ function Home(){
                         <h1 className="countpeople">320명</h1>
                     </div>
                 </div>
-                <div className='age'>
-                    <div className="mainage">
-                        <h3>현 방문자 수 .</h3>
-                        <h2><BsFillPeopleFill/></h2>
-                        <h1 className="countage">320명</h1>
+                <div className='mainage'>
+                    <div className="maintextage">
+                        <h3> 나이대</h3>
+                        <h2><AiOutlineUser/></h2>
+                        <h1 className="countage"><MdEmojiPeople/></h1>
+                        <h1 className="countagepertjs">성인</h1>
+                        <h1 className="countageperone">70%</h1>
+                        <h1 className="countagepericon"><MdEmojiPeople/></h1>
+                        <h1 className="countageperdjsl">어린이</h1>
+                        <h1 className="countageperchrild">30%</h1>
+                    </div>
+                </div>
+                <div className="maintime">
+                    <div className="maintime-gra">
+                        <h3>시간대</h3>
+                        <h2 className="maintimelogo"><AiOutlineClockCircle/></h2>
+                        <div className="timegraph" >
+                            <Rechart/>
+                        </div>
                     </div>
                 </div>
             </section>
