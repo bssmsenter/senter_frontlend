3cc3f0a1d250e0ed52efa63cad1b99ff481d9d36
jiseok12 <103105145+jiseok12@users.noreply.github.com>
2023-01-13 09:10:11 +0900

    set

--- src/components/time.js
+++ src/components/time.js
@@ -11,17 +11,17 @@ function Time() {
             <section className="time_total">
                 <div className="freq_t">
                     <h2 className="time_title">인구가 가장<br />많은 시간대</h2>
-                    <h3 className="freq_time">14:00</h3>
+                    <h3 className="freq_time">18:00</h3>
                 </div>
 
                 <div className="rare_t">
                     <h2 className="time_title">인구가 가장<br />적은 시간대</h2>
-                    <h3 className="rare_time">09:00</h3>
+                    <h3 className="rare_time">22:00</h3>
                 </div>
 
                 <div className="recomm_t">
                     <h2 className="time_title">추천<br />시간대</h2>
-                    <h3 className="rec_time">13:00</h3>
+                    <h3 className="rec_time">15:00</h3>
                 </div>
 
                 <div className="graph">
